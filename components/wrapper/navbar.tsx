defa7e375a8fe830d25653fdb743a4e5d99640e1
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-30 17:46:41 -0400

    Added github link

--- components/wrapper/navbar.tsx
+++ components/wrapper/navbar.tsx
@@ -1,35 +1,17 @@
 "use client"
-import Link from 'next/link';
-import * as React from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { Button } from "../ui/button";
-import { SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
-import { UserProfile } from "../user-profile";
-import ModeToggle from "../mode-toggle";
-import { BlocksIcon } from "lucide-react";
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
+import { NavigationMenu, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
 import config from "@/config";
 import { cn } from "@/lib/utils";
 import { useAuth } from "@clerk/nextjs";
 import { Dialog, DialogClose } from "@radix-ui/react-dialog";
-
-const components: { title: string; href: string; description: string }[] = [
-    {
-        title: "Marketing Page",
-        href: "/marketing-page",
-        description: "Write some wavy here to get them to click.",
-    },
-    {
-        title: "Marketing Page",
-        href: "/marketing-page",
-        description: "Write some wavy here to get them to click.",
-    },
-    {
-        title: "Marketing Page",
-        href: "/marketing-page",
-        description: "Write some wavy here to get them to click.",
-    },
-];
+import Link from 'next/link';
+import * as React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import ModeToggle from "../mode-toggle";
+import { Button } from "../ui/button";
+import { SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
+import { UserProfile } from "../user-profile";
+import { Github } from "lucide-react";
 
 export default function NavBar() {
     let userId = null;
@@ -78,35 +60,17 @@ export default function NavBar() {
                 </NavigationMenuList>
             </NavigationMenu>
             <div className="flex items-center gap-2 max-[825px]:hidden">
+                <Link
+                    href="https://github.com/michaelshimeles/component-renderer"
+                    target='_blank'
+                    className='animate-buttonheartbeat border p-2 rounded-full hover:dark:bg-black hover:cursor-pointer'
+                    aria-label="View Component Renderer's Code On Github"
+                >
+                    <Github className='w-4 h-4' aria-hidden="true" />
+                </Link>
                 {userId && <UserProfile />}
                 <ModeToggle />
             </div>
         </div>
     );
 }
-
-const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-    return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    );
-});
-ListItem.displayName = "ListItem";
